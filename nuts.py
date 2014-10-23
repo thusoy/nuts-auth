82d24ba66cfe3e60616672561693325c2e0f7574
Tarjei Husøy <git@thusoy.com>
2014-10-23 15:56:22 +0200

    Implement security associations

--- nuts.py
+++ nuts.py
@@ -1,8 +1,12 @@
 import hmac
-from collections import Namedtuple
+from collections import namedtuple
+from itsdangerous import constant_time_compare
+from functools import partial
 import os
+import json
 
 import hashlib
+import sha3
 
 def hkdf_extract(salt, input_key_material, hash=hashlib.sha512):
     '''
@@ -69,17 +73,46 @@ class Hkdf(object):
 
 # Store messages passed back and forth for inspection
 
-Message = Namedtuple('Message', ['dest', 'msg'])
+Message = namedtuple('Message', ['dest', 'msg'])
 _messages = []
 
 shared_key = shared_key = '39115349d0124171cccbdd46ce24c55f98a66809bff4f73d344abf81351a9ff6'.decode('hex')
 
 
 def send(dest, msg):
+    print 'Sending msg of length %d to %s' % (len(msg), dest)
     _messages.append( Message(dest, msg) )
+
+def mac(key, msg):
+    """ Create a secure MAC of the message with the key, using
+    Keccak (SHA-3) 512 truncated to 64 bits.
+    """
+#    import pdb; pdb.set_trace()
+    return hashlib.sha3_256(key + msg).digest()[:8]
+
+
+def get_mac(mac_name, mac_len):
+    """ Get the MAC function agreed upon in the SA. """
+    mac_func = getattr(hashlib, mac_name)
+    def _mac(key, msg):
+        return mac_func(key + msg).digest()[:mac_len]
+    return _mac
+
+
+class NUTSAuthException(Exception):
+    """ Base class for authentication-related exception in the auth channel. """
+
+class SignatureException(NUTSAuthException):
+    """ Invalid signature received. """
 
 
 class Connection(object):
+
+    supported_macs = [
+        'sha3_512',
+        'sha3_384',
+        'sha3_256',
+    ]
 
     def __init__(self, id_a, id_b, initial_msg_from_b):
         """ Establishing new connection to id_b, send a 128 bit response consisting of
@@ -91,15 +124,52 @@ class Connection(object):
         self.R_a = os.urandom(8)
         self.R_b = initial_msg_from_b
 
-        h = hmac.sha256(shared_key)
-        h.update(id_a)
-        h.update(id_b)
-        h.update(self.R_b)
-        a = self.R_a + h.digest()[:8]
+        msg = self.id_a + self.id_b + self.R_b
+        h = mac(shared_key, msg)
+        a = self.R_a + h
         send(id_b, a)
 
 
     def challenge_reply_received(self, response):
         """ Response received from id_b, that is H_k(id_a, id_b, R_a, a). """
-        session_key = Hkdf(shared_key + self.R_a + self.R_b)
-        send(self.id_b, b'Go ahead' + hmac.sha256(session_key).update('Go ahead').digest())
+        self.session_key = hkdf_expand(shared_key + self.R_a + self.R_b, length=16)
+        send(self.id_b, b'Go ahead' + mac(self.session_key, 'Go ahead'))
+
+
+    def sa_proposal_received(self, response):
+        msg, sig = response[:-8], response[-8:]
+        if not constant_time_compare(mac(self.session_key, msg), sig):
+            raise SignatureException()
+
+        msg_data = json.loads(msg)
+        suggested_macs = set(msg_data.get('macs', []))
+        for supported_mac in Connection.supported_macs:
+            if supported_mac in suggested_macs:
+                selected_mac = supported_mac
+                break
+        else:
+            raise "No MACs in common, aborting"
+        suggested_mac_len = msg_data.get('mac_len', 8)
+        try:
+            suggested_mac_len = int(suggested_mac_len)
+        except ValueError:
+            raise ValueError("Suggested mac_len not an integer, was %s" % suggested_mac_len)
+        if not (8 <= suggested_mac_len <= 32):
+            raise ValueError("suggested mac outside permitted range of 8-32 bytes")
+        # All jolly good, notify id_b of chosen MAC and signature length
+        response = {
+            'mac': selected_mac,
+            'mac_len': suggested_mac_len,
+        }
+        self.sa_mac_len = suggested_mac_len
+        response_msg = json.dumps(response)
+        mac_func = get_mac(selected_mac, suggested_mac_len)
+        self.mac = partial(mac_func, self.session_key)
+        send(self.id_b, response_msg + self.mac(response_msg))
+
+
+    def command_received(self, full_message):
+        """Signed, operational command received. Verify signature and return message."""
+        msg, sig = full_message[:-self.sa_mac_len], full_message[-self.sa_mac_len:]
+        if not constant_time_compare(msg, self.mac(msg)):
+            raise SignatureException()
--- test_connection.py
+++ test_connection.py
@@ -1,9 +1,9 @@
 # Expected comm as follows
 
 import os
-import hmac
+import json
 
-from nuts import Connection, _messages, send, Hkdf
+from nuts import Connection, _messages, send, hkdf_expand, mac, get_mac
 
 shared_key = '39115349d0124171cccbdd46ce24c55f98a66809bff4f73d344abf81351a9ff6'.decode('hex')
 
@@ -13,33 +13,59 @@ id_b = b'GroundStation-1'
 
 R_b = os.urandom(8)
 
-conn = Connection(shared_key, id_a, id_b, R_b)
+conn = Connection(id_a, id_b, R_b)
 
 # First message from sat should be 128 bits, and should verify the identify of the sat
 assert len(_messages[0].msg) == 16
 assert _messages[0].dest == id_b
-expected_hash_m1 = hmac.sha256(shared_key)
-expected_hash_m1.update(id_a)
-expected_hash_m1.update(id_b)
-expected_hash_m1.update(R_b)
-expected_m1_digest = expected_hash_m1.digest()
+msg = id_a + id_b + R_b
+expected_m1_digest = mac(shared_key, msg)
 
 # Message digest should be correct
 assert _messages[0].msg[8:] == expected_m1_digest
 
 # Prove our knowledge of shared_key
 R_a = _messages[0].msg[:8]
-h = hmac.sha256(shared_key)
-h.update(id_a)
-h.update(id_b)
-h.update(R_a)
-h.update(_messages[0].msg[8:])
-m2_digest = h.digest()[:8]
+msg = id_a + id_b + R_a
+m2_digest = mac(shared_key, msg)
 
 send(id_a, m2_digest)
 conn.challenge_reply_received(_messages[-1].msg)
-session_key = Hkdf(shared_key + R_a + R_b)
+session_key = hkdf_expand(shared_key + R_a + R_b, length=16)
 reply = 'Okay'
 
 # Assert both agreed on the same session key
-assert _messages[-1].msg = hmac.sha256(session_key).update('Go ahead').digest()
+assert _messages[-1].msg[:8] == 'Go ahead'
+assert _messages[-1].msg[len('Go ahead'):] == mac(session_key, 'Go ahead')
+
+sa_proposal = {
+    'macs': ['sha3_512'],
+    'mac_len': 8,
+}
+
+msg = json.dumps(sa_proposal)
+
+send(id_a, msg + mac(session_key, msg))
+
+conn.sa_proposal_received(_messages[-1].msg)
+
+# SA response
+proposal_response = json.loads(_messages[-1].msg[:-8])
+
+# Should agree on sha3_512
+assert proposal_response['mac'] == 'sha3_512'
+
+conn_mac = get_mac(proposal_response['mac'], proposal_response['mac_len'])
+
+# Should be valid signature
+assert _messages[-1].msg[-8:] == conn_mac(session_key, _messages[-1].msg[:-8])
+
+
+# Send first actual command
+cmd = {'cmd': 'TakePicture'}
+cmd_msg = json.dumps(cmd)
+send(id_a, cmd_msg + conn_mac(session_key, cmd_msg))
+
+
+
+
